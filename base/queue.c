f91d854b889b6a86405a31cd3e81602247c96a28
王锋 <wangfeng@yeah.net>
2014-12-31 12:06:43 +0800

    修订队列的修饰符

--- base/queue.c
+++ base/queue.c
@@ -1,44 +1,35 @@
-
-/*
- * Copyright (C) Igor Sysoev
- * Copyright (C) Nginx, Inc.
- */
-
-
-#include <ngx_config.h>
-#include <ngx_core.h>
-
+#include <api/queue.h>
 
 /*
  * find the middle queue element if the queue has odd number of elements
  * or the first element of the queue's second part otherwise
  */
 
-ngx_queue_t *
-ngx_queue_middle(ngx_queue_t *queue)
+api_queue_t *
+api_queue_middle(api_queue_t *queue)
 {
-    ngx_queue_t  *middle, *next;
+    api_queue_t  *middle, *next;
 
-    middle = ngx_queue_head(queue);
+    middle = api_queue_head(queue);
 
-    if (middle == ngx_queue_last(queue)) {
+    if (middle == api_queue_last(queue)) {
         return middle;
     }
 
-    next = ngx_queue_head(queue);
+    next = api_queue_head(queue);
 
     for ( ;; ) {
-        middle = ngx_queue_next(middle);
+        middle = api_queue_next(middle);
 
-        next = ngx_queue_next(next);
+        next = api_queue_next(next);
 
-        if (next == ngx_queue_last(queue)) {
+        if (next == api_queue_last(queue)) {
             return middle;
         }
 
-        next = ngx_queue_next(next);
+        next = api_queue_next(next);
 
-        if (next == ngx_queue_last(queue)) {
+        if (next == api_queue_last(queue)) {
             return middle;
         }
     }
@@ -48,33 +39,33 @@ ngx_queue_middle(ngx_queue_t *queue)
 /* the stable insertion sort */
 
 void
-ngx_queue_sort(ngx_queue_t *queue,
-    ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *))
+api_queue_sort(api_queue_t *queue,
+    api_int_t (*cmp)(const api_queue_t *, const api_queue_t *))
 {
-    ngx_queue_t  *q, *prev, *next;
+    api_queue_t  *q, *prev, *next;
 
-    q = ngx_queue_head(queue);
+    q = api_queue_head(queue);
 
-    if (q == ngx_queue_last(queue)) {
+    if (q == api_queue_last(queue)) {
         return;
     }
 
-    for (q = ngx_queue_next(q); q != ngx_queue_sentinel(queue); q = next) {
+    for (q = api_queue_next(q); q != api_queue_sentinel(queue); q = next) {
 
-        prev = ngx_queue_prev(q);
-        next = ngx_queue_next(q);
+        prev = api_queue_prev(q);
+        next = api_queue_next(q);
 
-        ngx_queue_remove(q);
+        api_queue_remove(q);
 
         do {
             if (cmp(prev, q) <= 0) {
                 break;
             }
 
-            prev = ngx_queue_prev(prev);
+            prev = api_queue_prev(prev);
 
-        } while (prev != ngx_queue_sentinel(queue));
+        } while (prev != api_queue_sentinel(queue));
 
-        ngx_queue_insert_after(prev, q);
+        api_queue_insert_after(prev, q);
     }
 }
--- include/api/queue.h
+++ include/api/queue.h
@@ -1,78 +1,66 @@
 #ifndef __API_QUEUE_H_INCLUDED__
 #define __API_QUEUE_H_INCLUDED__
 
-typedef struct ngx_queue_s  ngx_queue_t;
+typedef struct api_queue_s  api_queue_t;
 
-struct ngx_queue_s {
-    ngx_queue_t  *prev;
-    ngx_queue_t  *next;
+struct api_queue_s {
+    api_queue_t  *prev;
+    api_queue_t  *next;
 };
 
 
-#define ngx_queue_init(q)                                                     \
+#define api_queue_init(q)                                                     \
     (q)->prev = q;                                                            \
     (q)->next = q
 
 
-#define ngx_queue_empty(h)                                                    \
+#define api_queue_empty(h)                                                    \
     (h == (h)->prev)
 
 
-#define ngx_queue_insert_head(h, x)                                           \
+#define api_queue_insert_head(h, x)                                           \
     (x)->next = (h)->next;                                                    \
     (x)->next->prev = x;                                                      \
     (x)->prev = h;                                                            \
     (h)->next = x
 
 
-#define ngx_queue_insert_after   ngx_queue_insert_head
+#define api_queue_insert_after   api_queue_insert_head
 
 
-#define ngx_queue_insert_tail(h, x)                                           \
+#define api_queue_insert_tail(h, x)                                           \
     (x)->prev = (h)->prev;                                                    \
     (x)->prev->next = x;                                                      \
     (x)->next = h;                                                            \
     (h)->prev = x
 
 
-#define ngx_queue_head(h)                                                     \
+#define api_queue_head(h)                                                     \
     (h)->next
 
 
-#define ngx_queue_last(h)                                                     \
+#define api_queue_last(h)                                                     \
     (h)->prev
 
 
-#define ngx_queue_sentinel(h)                                                 \
+#define api_queue_sentinel(h)                                                 \
     (h)
 
 
-#define ngx_queue_next(q)                                                     \
+#define api_queue_next(q)                                                     \
     (q)->next
 
 
-#define ngx_queue_prev(q)                                                     \
+#define api_queue_prev(q)                                                     \
     (q)->prev
 
 
-#if (NGX_DEBUG)
-
-#define ngx_queue_remove(x)                                                   \
-    (x)->next->prev = (x)->prev;                                              \
-    (x)->prev->next = (x)->next;                                              \
-    (x)->prev = NULL;                                                         \
-    (x)->next = NULL
-
-#else
-
-#define ngx_queue_remove(x)                                                   \
+#define api_queue_remove(x)                                                   \
     (x)->next->prev = (x)->prev;                                              \
     (x)->prev->next = (x)->next
 
-#endif
 
-
-#define ngx_queue_split(h, q, n)                                              \
+#define api_queue_split(h, q, n)                                              \
     (n)->prev = (h)->prev;                                                    \
     (n)->prev->next = n;                                                      \
     (n)->next = q;                                                            \
@@ -81,20 +69,20 @@ struct ngx_queue_s {
     (q)->prev = n;
 
 
-#define ngx_queue_add(h, n)                                                   \
+#define api_queue_add(h, n)                                                   \
     (h)->prev->next = (n)->next;                                              \
     (n)->next->prev = (h)->prev;                                              \
     (h)->prev = (n)->prev;                                                    \
     (h)->prev->next = h;
 
 
-#define ngx_queue_data(q, type, link)                                         \
+#define api_queue_data(q, type, link)                                         \
     (type *) ((u_char *) q - offsetof(type, link))
 
 
-ngx_queue_t *ngx_queue_middle(ngx_queue_t *queue);
-void ngx_queue_sort(ngx_queue_t *queue,
-    ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *));
+API api_queue_t *api_queue_middle(api_queue_t *queue);
+API void api_queue_sort(api_queue_t *queue,
+    api_int_t (*cmp)(const api_queue_t *, const api_queue_t *));
 
 
 #endif /* ! __API_QUEUE_H_INCLUDED_ */
