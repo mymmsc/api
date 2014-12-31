bfe56cceab55b3e420ac3eb78fd6cee2ba206295
王锋 <wangfeng@yeah.net>
2014-12-31 10:21:19 +0800

    同步锁输出调试信息

--- base/shmtx.c
+++ base/shmtx.c
@@ -46,6 +46,7 @@ api_shmtx_lock(api_shmtx_t *mtx)
     api_uint_t         i, n;
 
     for ( ;; ) {
+		printf("api_pid: %d\n", api_pid);
         if (*mtx->lock == 0 && api_atomic_cmp_set(mtx->lock, 0, api_pid)) {
             return;
         }
