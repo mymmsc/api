dd6c0f98fad40a330001a05c3d29da4d72339cd5
王锋 <wangfeng@yeah.net>
2014-12-31 08:41:26 +0800

    输出调试信息

--- base/slab.c
+++ base/slab.c
@@ -97,9 +97,10 @@ static void slab_init(api_slab_pool_t *pool)
     p += n * sizeof(api_slab_page_t);
 
     pages = (api_uint_t) (size / (api_pagesize + sizeof(api_slab_page_t)));
-
+    printf("pages: %d\n", pages);
     api_memzero(p, pages * sizeof(api_slab_page_t));
-
+    printf("1-1\n");
+    
     pool->pages = (api_slab_page_t *) p;
 
     pool->free.prev = 0;
@@ -109,10 +110,11 @@ static void slab_init(api_slab_pool_t *pool)
     pool->pages->next = &pool->free;
     pool->pages->prev = (uintptr_t) &pool->free;
 
+    printf("2-1\n");
     pool->start = (uint8_t *)
                   api_align_ptr((uintptr_t) p + pages * sizeof(api_slab_page_t),
                                  api_pagesize);
-
+    printf("3-1\n");
     m = pages - (pool->end - pool->start) / api_pagesize;
     if (m > 0) {
         pages -= m;
@@ -124,6 +126,7 @@ static void slab_init(api_slab_pool_t *pool)
     pool->log_nomem = 1;
     pool->log_ctx = &pool->zero;
     pool->zero = '\0';
+    printf("exit...\n"); 
 }
 
 api_slab_pool_t *api_slab_init(void *addr, size_t size)
